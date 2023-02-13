f669dea97253c5a565881adde768cf011ed3b77d
rhuibertsjr <rhuibertsjr@gmail.com>
2023-02-13 21:46:07 +0100

    Added: Org note-taking (Org-roam v2)

---
+++ functions/note-taking.el
@@ -0,0 +1,34 @@
+;;; note-taking.el --- rhuibjr's org-roam config     -*- lexical-binding: t; -*-
+;;
+;; Author: Rhuibjr
+;; Maintainer: Rhuibjr <rhuibjr.business@gmail.com>
+;;
+;; Copyright (C) 2022, Rhuibjr, all rights reserved.
+;;
+;;;
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;;; Package
+;;
+(setq
+  org-roam-v2-ack t
+  org-roam-completion-everywhere t)
+
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory (file-truename "/Users/renehuiberts/Documentations/wiki/"))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert))
+  :config
+  (org-roam-setup))
